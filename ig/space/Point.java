4b6b87b70b251d3e683b0afd765a14805235057a
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2022-05-22 09:40:38 -0300

    Point01: added point class

---
+++ ig/space/Point.java
@@ -0,0 +1,29 @@
+package ig.space;
+
+public class Point {
+
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        setX(x);
+        setY(y);
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+}
