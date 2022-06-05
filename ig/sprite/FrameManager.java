8335406dd4a24527e1313e49e35112c24df6dda5
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2022-06-05 17:32:26 -0300

    FrameManager02: started framemanager doc

--- ig/sprite/FrameManager.java
+++ ig/sprite/FrameManager.java
@@ -1,27 +1,88 @@
 package ig.sprite;
 
+/**
+ * Class that helps managing the
+ * frames of a {@code SpriteSheet}.
+ * 
+ * @author Daniel O Sousa
+ */
 public class FrameManager {
 
+    /**
+     * The {@code SpriteSheet}
+     * instance that is managed
+     * by this {@code FrameManager}.
+     */
     private SpriteSheet spriteSheet;
+
+    /**
+     * The column of the
+     * {@code spriteSheet}
+     * where to select a
+     * frame from.
+     */
     private int frameX = 0;
+
+    /**
+     * The row of the
+     * {@code spriteSheet}
+     * where to select a
+     * frame from.
+     */
     private int frameY = 0;
+
+    /**
+     * The {@code spriteSheet}'s
+     * frame which is currently
+     * selected.
+     */
     private Sprite currentFrame;
 
     // TODO throw an exception if spriteSheet is null
+    /**
+     * Creates a {@code FrameManager} instance
+     * that will manage the frames of the
+     * passed {@code SpriteSheet}.
+     * 
+     * @param spriteSheet the {@code SpriteSheet}
+     *                    to be managed
+     */
     public FrameManager(SpriteSheet spriteSheet) {
         storeSpriteSheet(spriteSheet);
         setFrameX(0);
         setFrameY(0);
     }
 
+    /**
+     * Stores the passed {@code SpriteSheet} into
+     * the {@code spriteSheet} property.
+     * 
+     * @param spriteSheet the new {@code SpriteSheet}
+     *                    that will be managed by this
+     *                    {@code FrameManager}
+     */
     private void storeSpriteSheet(SpriteSheet spriteSheet) {
         this.spriteSheet = spriteSheet;
     }
 
+    /**
+     * Returns the {@code SpriteSheet}
+     * managed by this {@code FrameManager}.
+     * 
+     * @return the {@code spriteSheet} of
+     *         this {@code FrameManager}
+     */
     public SpriteSheet getSpriteSheet() {
         return this.spriteSheet;
     }
 
+    /**
+     * Sets the column of this {@code FrameManager}'s
+     * {@code spriteSheet} to select a frame from.
+     * 
+     * @param frameX the column where to get a
+     *               frame from
+     */
     public void setFrameX(int frameX) {
         if(frameX >= 0 && frameX < spriteSheet.getColumns()) {
             this.frameX = frameX;
@@ -49,6 +110,13 @@ public class FrameManager {
         return this.frameX;
     }
 
+    /**
+     * Sets the row of this {@code FrameManager}'s
+     * {@code spriteSheet} to select a frame from.
+     * 
+     * @param frameY the row where to get a
+     *               frame from
+     */
     public void setFrameY(int frameY) {
         if(frameY >= 0 && frameY < spriteSheet.getRows()) {
             this.frameY = frameY;
