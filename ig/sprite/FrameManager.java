9bc67c3dd99663f928ce323bdbc39ae7c9ec1b44
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2022-06-05 19:55:59 -0300

    FrameManager03: finished framemanager doc

--- ig/sprite/FrameManager.java
+++ ig/sprite/FrameManager.java
@@ -32,9 +32,9 @@ public class FrameManager {
     private int frameY = 0;
 
     /**
-     * The {@code spriteSheet}'s
-     * frame which is currently
-     * selected.
+     * The currently selected frame
+     * from this {@code FrameManager}'s
+     * {@code spriteSheet}.
      */
     private Sprite currentFrame;
 
@@ -57,7 +57,7 @@ public class FrameManager {
      * Stores the passed {@code SpriteSheet} into
      * the {@code spriteSheet} property.
      * 
-     * @param spriteSheet the new {@code SpriteSheet}
+     * @param spriteSheet the {@code SpriteSheet}
      *                    that will be managed by this
      *                    {@code FrameManager}
      */
@@ -78,7 +78,14 @@ public class FrameManager {
 
     /**
      * Sets the column of this {@code FrameManager}'s
-     * {@code spriteSheet} to select a frame from.
+     * {@code spriteSheet} to select a frame from and
+     * selects the frame which is there.
+     * <p>
+     * If the passed {@code frameX} is not a column of the
+     * {@code spriteSheet} (i.e. is less than {@code 0} or greater
+     * than the number of columns of the {@code spriteSheet}
+     * - {@code 1}), then this method won't
+     * do anything.
      * 
      * @param frameX the column where to get a
      *               frame from
@@ -90,6 +97,11 @@ public class FrameManager {
         }
     }
 
+    /**
+     * Selects the frame at the next column
+     * of this {@code FrameManager}'s
+     * {@code spriteSheet}.
+     */
     public void nextFrameX() {
         if(frameX < spriteSheet.getColumns() - 1) {
             setFrameX(frameX + 1);
@@ -98,6 +110,11 @@ public class FrameManager {
         }
     }
 
+    /**
+     * Selects the frame at the previous column
+     * of this {@code FrameManager}'s
+     * {@code spriteSheet}.
+     */
     public void previousFrameX() {
         if(frameX > 0) {
             setFrameX(frameX - 1);
@@ -106,13 +123,28 @@ public class FrameManager {
         }
     }
 
+    /**
+     * Returns the column of
+     * this {@code FrameManager}'s
+     * {@code spriteSheet} where the
+     * current frame is located.
+     * 
+     * @return the column of the current frame
+     */
     public int getFrameX() {
         return this.frameX;
     }
 
     /**
      * Sets the row of this {@code FrameManager}'s
-     * {@code spriteSheet} to select a frame from.
+     * {@code spriteSheet} to select a frame from and
+     * selects the frame which is there.
+     * <p>
+     * If the passed {@code frameY} is not a row of the
+     * {@code spriteSheet} (i.e. is less than {@code 0} or greater
+     * than the number of rows of the {@code spriteSheet}
+     * - {@code 1}), then this method won't
+     * do anything.
      * 
      * @param frameY the row where to get a
      *               frame from
@@ -124,6 +156,11 @@ public class FrameManager {
         }
     }
 
+    /**
+     * Selects the frame at the next row
+     * of this {@code FrameManager}'s
+     * {@code spriteSheet}.
+     */
     public void nextFrameY() {
         if(frameY < spriteSheet.getRows() - 1) {
             setFrameY(frameY + 1);
@@ -132,6 +169,11 @@ public class FrameManager {
         }
     }
 
+    /**
+     * Selects the frame at the previous row
+     * of this {@code FrameManager}'s
+     * {@code spriteSheet}.
+     */
     public void previousFrameY() {
         if(frameY > 0) {
             setFrameY(frameY - 1);
@@ -140,14 +182,33 @@ public class FrameManager {
         }
     }
 
+    /**
+     * Returns the row of
+     * this {@code FrameManager}'s
+     * {@code spriteSheet} where the
+     * current frame is located.
+     * 
+     * @return the row of the current frame
+     */
     public int getFrameY() {
         return this.frameY;
     }
 
+    /**
+     * Updates the {@code currentFrame} to
+     * match the frame specified by the {@code frameX}
+     * and {@code frameY} properties.
+     */
     private void updateCurrentFrame() {
         this.currentFrame = spriteSheet.getSprites()[frameY][frameX];
     }
 
+    /**
+     * Returns the current frame selected
+     * by this {@code FrameManager}.
+     * 
+     * @return the current frame of this {@code FrameManager}
+     */
     public Sprite getCurrentFrame() {
         return this.currentFrame;
     }
