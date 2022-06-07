bef1b33182423bc2f4de6fd06ab1750aa42d9887
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2022-06-07 18:12:36 -0300

    Stage02: started integration with scenario class

--- ig/stage/Stage.java
+++ ig/stage/Stage.java
@@ -3,8 +3,12 @@ package ig.stage;
 import java.util.ArrayList;
 import java.awt.Graphics2D;
 
+import ig.scenario.Scenario;
 import ig.object.GameObject;
 
+// TODO prevent errors from accessing scenario arraylists incorrectly
+// TODO draw backgrounds and foregrounds
+// TODO document
 /**
  * Class for creating a stage for a {@code Game}.
  * 
@@ -12,17 +16,29 @@ import ig.object.GameObject;
  */
 public class Stage {
 
-    /** The title of the stage. */
+    /**
+     * The title of the stage.
+     */
     private String title;
 
-    /** The width of the stage. */
+    /**
+     * The width of the stage.
+     */
     private int width;
 
-    /** The height of the stage. */
+    /**
+     * The height of the stage.
+     */
     private int height;
 
-    /** The objects of the stage. */
+    private ArrayList<Scenario> backgrounds = new ArrayList<Scenario>();
+
+    /**
+     * The objects of the stage.
+     */
     private ArrayList<GameObject> objects = new ArrayList<GameObject>();
+
+    private ArrayList<Scenario> foregrounds = new ArrayList<Scenario>();
 
     /**
      * Constructor for a {@code Stage}.
@@ -104,6 +120,70 @@ public class Stage {
     public void setDimensions(int width, int height) {
         setWidth(width);
         setHeight(height);
+    }
+
+    public void addFurtherBackground(Scenario background) {
+        backgrounds.add(background);
+    }
+
+    public void addNthBackground(int position, Scenario background) {
+        backgrounds.add(position, background);
+    }
+
+    public void addCloserBackground(Scenario background) {
+        backgrounds.add(0, background);
+    }
+
+    public Scenario removeFurtherBackground() {
+        return backgrounds.remove(backgrounds.size() - 1);
+    }
+
+    public Scenario removeNthBackground(int position) {
+        return backgrounds.remove(position);
+    }
+
+    public void removeBackground(Scenario background) {
+        backgrounds.remove(background);
+    }
+
+    public Scenario removeCloserBackground() {
+        return backgrounds.remove(0);
+    }
+
+    public ArrayList<Scenario> getBackgrounds() {
+        return backgrounds;
+    }
+
+    public void addFurtherForeground(Scenario foreground) {
+        foregrounds.add(foreground);
+    }
+
+    public void addNthForeground(int position, Scenario foreground) {
+        foregrounds.add(position, foreground);
+    }
+
+    public void addCloserForeground(Scenario foreground) {
+        foregrounds.add(0, foreground);
+    }
+
+    public Scenario removeFurtherForeground() {
+        return foregrounds.remove(foregrounds.size() - 1);
+    }
+
+    public Scenario removeNthForeground(int position) {
+        return foregrounds.remove(position);
+    }
+
+    public void removeForeground(Scenario foreground) {
+        foregrounds.remove(foreground);
+    }
+
+    public Scenario removeCloserForeground() {
+        return foregrounds.remove(0);
+    }
+
+    public ArrayList<Scenario> getForegrounds() {
+        return foregrounds;
     }
 
     /**
