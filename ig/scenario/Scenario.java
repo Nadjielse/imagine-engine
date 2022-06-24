c0d20ac3e656c5162c7b0ebc547ea90e1dec3429
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2022-06-24 14:59:07 -0300

    GameObject03&Scenario02: gameobject and scenario now extend gameelement

--- ig/scenario/Scenario.java
+++ ig/scenario/Scenario.java
@@ -1,122 +1,24 @@
 package ig.scenario;
 
-import ig.sprite.FrameManager;
+import ig.game.GameElement;
 import ig.sprite.SpriteSheet;
-import ig.sprite.Sprite;
 
-public class Scenario {
+public class Scenario extends GameElement {
 
-    private int x;
-    private int y;
-    private int width;
-    private int height;
-    private FrameManager frameManager;
     private boolean repeatX = true;
     private boolean repeatY = true;
-    private int xSpeed = 0;
-    private int ySpeed = 0;
 
     // TODO constructor should not accept null spriteSheet
     public Scenario(SpriteSheet spriteSheet) {
         setCoordinates(0, 0);
         setSize(spriteSheet.getSpriteWidth(), spriteSheet.getSpriteHeight());
-        createFrameManager(spriteSheet);
+        setSpriteSheet(spriteSheet);
     }
 
     public Scenario(SpriteSheet spriteSheet, int width, int height) {
         setCoordinates(0, 0);
         setSize(width, height);
-        createFrameManager(spriteSheet);
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getX() {
-        return this.x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public int getY() {
-        return this.y;
-    }
-
-    public void setCoordinates(int x, int y) {
-        setX(x);
-        setY(y);
-    }
-
-    public void setWidth(int width) {
-        if(width >= 0) {
-            this.width = width;
-        }
-    }
-
-    public int getWidth() {
-        return this.width;
-    }
-
-    public void setHeight(int height) {
-        if(height >= 0) {
-            this.height = height;
-        }
-    }
-
-    public int getHeight() {
-        return this.height;
-    }
-
-    public void setSize(int width, int height) {
-        setWidth(width);
-        setHeight(height);
-    }
-
-    private void createFrameManager(SpriteSheet spriteSheet) {
-        frameManager = new FrameManager(spriteSheet);
-    }
-
-    public SpriteSheet getSpriteSheet() {
-        return frameManager.getSpriteSheet();
-    }
-
-    public void setFrameX(int frameX) {
-        frameManager.setFrameX(frameX);
-    }
-
-    public void nextFrameX() {
-        frameManager.nextFrameX();
-    }
-
-    public void previousFrameX() {
-        frameManager.previousFrameX();
-    }
-
-    public int getFrameX() {
-        return frameManager.getFrameX();
-    }
-
-    public void setFrameY(int frameY) {
-        frameManager.setFrameY(frameY);
-    }
-
-    public void nextFrameY() {
-        frameManager.nextFrameY();
-    }
-
-    public void previousFrameY() {
-        frameManager.previousFrameY();
-    }
-
-    public int getFrameY() {
-        return frameManager.getFrameY();
-    }
-
-    public Sprite getCurrentFrame() {
-        return frameManager.getCurrentFrame();
+        setSpriteSheet(spriteSheet);
     }
 
     public void setRepeatX(boolean repeatX) {
@@ -135,20 +37,4 @@ public class Scenario {
         return this.repeatY;
     }
 
-    public void setXSpeed(int xSpeed) {
-        this.xSpeed = xSpeed;
-    }
-
-    public int getXSpeed() {
-        return this.xSpeed;
-    }
-
-    public void setYSpeed(int ySpeed) {
-        this.ySpeed = ySpeed;
-    }
-
-    public int getYSpeed() {
-        return this.ySpeed;
-    }
-    
 }
