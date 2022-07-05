89d53dc555b7d2dc73e480368fe8bf626820b229
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2022-07-05 15:10:08 -0300

    code: documented scenario types

--- ig/scenario/type/RepeatXY.java
+++ ig/scenario/type/RepeatXY.java
@@ -4,15 +4,47 @@ import ig.scenario.Scenario;
 
 import java.awt.Graphics2D;
 
+/**
+ * Class to represent a {@code ScenarioType}
+ * that has as characteristic repeat itself
+ * on the x and y axis to fill the {@code GamePanel}.
+ * 
+ * @author Daniel O Sousa
+ */
 public class RepeatXY implements ScenarioType {
 
+    /**
+     * Field that stores the name of
+     * this {@code ScenarioType}.
+     */
     private String name = "RepeatXY";
+
+    /**
+     * The {@code Scenario} that has
+     * this {@code ScenarioType}.
+     */
     private Scenario scenario;
 
+    /**
+     * Constructs a new {@code RepeatXY}
+     * {@code ScenarioType} that characterizes
+     * the {@code Scenario} passed via the
+     * {@code scenario} parameter.
+     * 
+     * @param scenario the scenario that
+     * has this type
+     */
     public RepeatXY(Scenario scenario) {
         storeScenario(scenario);
     }
 
+    /**
+     * Stores the {@code scenario} parameter
+     * in the {@code scenario} field.
+     * 
+     * @param scenario the {@code scenario}
+     * to be stored.
+     */
     private void storeScenario(Scenario scenario) {
         if(scenario == null) {
             throw new IllegalArgumentException("scenario cannot be null");
@@ -21,11 +53,27 @@ public class RepeatXY implements ScenarioType {
         this.scenario = scenario;
     }
 
+    /**
+     * Returns the name of this
+     * {@code ScenarioType}.
+     * 
+     * @return the name of this
+     * {@code ScenarioType}
+     */
     @Override
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Draws the {@code Scenario} that has this
+     * {@code ScenarioType} in a way that it repeats
+     * itself to fill the {@code GamePanel} on the
+     * x and y axis.
+     * 
+     * @param g2 a {@code Graphics2D} instance used
+     * for drawing the said {@code Scenario}
+     */
     @Override
     public void draw(Graphics2D g2) {
         if(scenario.getGamePanelWidth() == 0 || scenario.getGamePanelHeight() == 0) {
