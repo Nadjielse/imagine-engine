20cd6ad0502dd97343e380fe0b240f304350f4c3
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2022-07-15 20:01:39 -0300

    CameraType01: documented cameratype class

--- ig/camera/type/CameraType.java
+++ ig/camera/type/CameraType.java
@@ -4,12 +4,38 @@ import java.awt.Graphics2D;
 
 import ig.camera.CameraGrid;
 
+/**
+ * Interface extended by all
+ * {@code CameraType}s.
+ */
 public interface CameraType {
 
+    /**
+     * Method for setting the grid
+     * of the {@code Camera} that has
+     * this {@code CameraType}.
+     * 
+     * @param grid the grid to be set
+     */
     void setGrid(CameraGrid grid);
     
+    /**
+     * Method for defining how
+     * a {@code Camera} that has
+     * this {@code CameraType}
+     * should update.
+     */
     void update();
 
+    /**
+     * Method for defining how
+     * a {@code Camera} that has
+     * this {@code CameraType}
+     * should draw its grid.
+     * 
+     * @param g2 {@code Graphics2D}
+     * instance to draw with
+     */
     void draw(Graphics2D g2);
 
 }
